9e379141c4d1c3f904eb1f1129184a881cf3e1b5
LucasGinard <contactolucasginard@gmail.com>
2024-08-12 22:48:47 -0400

    add group endpoints

--- app/service.py
+++ app/service.py
@@ -34,7 +34,6 @@ app.add_middleware(
     allow_credentials=True,
 )
 
-
 @app.on_event("startup")
 async def startup():
     await db.connect()
@@ -45,34 +44,12 @@ async def shutdown():
     await db.disconnect()
 
 
-@app.post("/api/v1/providers", response_model=schemas.APIKey)
-async def create_provider(
-    provider: schemas.Provider, key: APIKey = Depends(validate_master_key)
-):
-    key = await models.Provider.create_new_key(db, provider.provider)
-    return schemas.APIKey(key=key)
-
-
-@app.get("/api/v1/providers")
-async def list_providers(key: APIKey = Depends(validate_master_key)):
-    return [
-        schemas.Provider.from_orm(s) for s in await models.Provider.get_providers(db)
-    ]
-
-
-@app.delete("/api/v1/providers/{provider}")
-async def delete_provider(provider: str, key: APIKey = Depends(validate_master_key)):
-    return await models.Provider.revoke_all_keys(db, provider)
-
-
-@app.post("/api/v1/measurements")
+@app.post("/api/v1/measurements", tags=[models.TagsEnum.apiKey])
 async def post(
     measurements: List[schemas.Measurement], provider: str = Depends(validate_api_key)
 ):
     await models.Measurement.store(db, [m.to_orm(provider) for m in measurements])
-
-
-@app.get("/api/v1/measurements", response_model=List[schemas.Measurement])
+@app.get("/api/v1/measurements", response_model=List[schemas.Measurement], tags=[models.TagsEnum.public])
 async def get(query: schemas.QueryParams = Depends(schemas.QueryParams)):
     return [
         schemas.Measurement.from_orm(m)
@@ -80,17 +57,17 @@ async def get(query: schemas.QueryParams = Depends(schemas.QueryParams)):
     ]
 
 
-@app.get("/api/v1/aqi", response_model=List[schemas.Report])
+@app.get("/api/v1/aqi", response_model=List[schemas.Report], tags=[models.TagsEnum.public])
 async def aqi(query: schemas.QueryParams = Depends(schemas.QueryParams)):
     return await reports.AQI.generate(db, query)
 
 
-@app.get("/api/v1/stats", response_model=List[schemas.ReportStats])
+@app.get("/api/v1/stats", response_model=List[schemas.ReportStats], tags=[models.TagsEnum.public])
 async def stats(query: schemas.QueryParams = Depends(schemas.QueryParams)):
     return await reports.Stats.generate(db, query)
 
 
-@app.get("/api/v1/status", response_model=schemas.ServiceStatus)
+@app.get("/api/v1/status", response_model=schemas.ServiceStatus, tags=[models.TagsEnum.public])
 async def status():
     status = schemas.ServiceStatus()
 
@@ -98,3 +75,22 @@ async def status():
         status.database = schemas.Status.DOWN
 
     return status
+
+@app.post("/api/v1/providers", response_model=schemas.APIKey, tags=[models.TagsEnum.apiKeyMaster])
+async def create_provider(
+    provider: schemas.Provider, key: APIKey = Depends(validate_master_key)
+):
+    key = await models.Provider.create_new_key(db, provider.provider)
+    return schemas.APIKey(key=key)
+
+
+@app.get("/api/v1/providers", tags=[models.TagsEnum.apiKeyMaster])
+async def list_providers(key: APIKey = Depends(validate_master_key)):
+    return [
+        schemas.Provider.from_orm(s) for s in await models.Provider.get_providers(db)
+    ]
+
+
+@app.delete("/api/v1/providers/{provider}", tags=[models.TagsEnum.apiKeyMaster])
+async def delete_provider(provider: str, key: APIKey = Depends(validate_master_key)):
+    return await models.Provider.revoke_all_keys(db, provider)
